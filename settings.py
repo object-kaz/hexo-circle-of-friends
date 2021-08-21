62acc34f8c65089929ab1b2994d529c5e568966d
Kaz <54239670+object-kaz@users.noreply.github.com>
2021-08-21 23:30:50 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -63,10 +63,10 @@ GITHUB_FRIENDS_LINKS = {
 
 # gitee友链配置
 GITEE_FRIENDS_LINKS = {
-    "enable": False,    # True 开启gitee issue兼容
-    "type": "normal",  # volantis用户请在这里填写 volantis
-    "owner": "Zfour",  # 填写你的github用户名
-    "repo": "zfour-friends",  # 填写你的github仓库名
+    "enable": True,    # True 开启gitee issue兼容
+    "type": "volantis",  # volantis用户请在这里填写 volantis
+    "owner": "pikoyo",  # 填写你的github用户名
+    "repo": "hexo-friends",  # 填写你的github仓库名
     "state": "open"  # 填写抓取的issue状态(open/closed)
 }
 
